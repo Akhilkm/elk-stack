02b7413fe496803cb4d08f87170e1bcd25c4f348
Akhil KM <akhilu011@gmail.com>
2020-04-25 15:31:37 +0530

    added configuration file and startup script

---
+++ dockerfiles/Elasticsearch/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Starting Elasticsearch ${ES_VERSION}"
+
+${ES_HOME}/bin/elasticsearch
---
+++ dockerfiles/Kibana/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Starting Kibana ${ES_VERSION}"
+
+${KIBANA_HOME}/bin/kibana
